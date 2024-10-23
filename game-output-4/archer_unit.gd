59a78996dd4cf3ab3c94fbe24cfa919b657edc6f
andrewvillorente1 <andrew.villorente@student.ateneo.edu>
2024-10-23 22:43:46 +0800

    UPDATED WITH SAMPLE BATTLE SCENE

--- game-output-4/archer_unit.gd
+++ game-output-4/archer_unit.gd
@@ -1,12 +1,11 @@
 extends Node2D
 
 var health = 110
-var attack_1_damage = 30
-var attack_2_damage = 45
-var attack_3_damage = 65
-
-# Animations
-@onready var animated_sprite = $AnimatedSprite2D
+var multiplier_counter = 0
+var attack_multiplier = 1
+var debuff_turn_counter = 0
+var is_animating = false
+@onready var animated_sprite = $ArcherSprite2D
 
 # Called when the node enters the scene tree for the first time
 func _ready() -> void:
@@ -14,35 +13,67 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	animated_sprite.play("idle")
+	if not is_animating and animated_sprite.animation != "archer_idle":
+		animated_sprite.play("archer_idle")
+
 
 # Single attack function with an integer determining the type of attack
 func attack(attack_type: int) -> int:
+	multiplier_counter += 1
+	var attack_1_damage = 35 * attack_multiplier
+	var attack_2_damage = 40 * attack_multiplier
 	match attack_type:
 		1:
-			animated_sprite.play("archer_attack_1")
+			play_animation("archer_attack_1")
 			return attack_1_damage
 		2:
-			animated_sprite.play("archer_attack_2")
+			play_animation("archer_attack_2")
 			return attack_2_damage
-		3:
-			animated_sprite.play("archer_attack_3")
-			return attack_3_damage
 		_:
 			print("Invalid attack type")
 			return 0
 
-func defend_1():
-	animated_sprite.play("archer_defend_1")
+func attack_multiplier_roll():
+	var random_value = randi() % 100  # Generate a random number between 0 and 99
+	if random_value < 10:  # 0 to 9 (10% chance)
+		attack_multiplier = 2
+	else:  # 10 to 99 (90% chance)
+		attack_multiplier = 1
 
-func defend_2():
-	animated_sprite.play("archer_defend_2")
+func return_multiplier_count() -> int:
+	return multiplier_counter
 
 func take_damage(amount: int):
 	health -= amount
 	if health <= 0:
 		die()
 
+func get_poisoned(amount: int):
+	play_animation("archer_poisoned")
+	for i in range (10): #The reason why it's in a for loop is because we can have a timer after health is reduced so in the Game scene, assuming there's a live view of unit health, we'll see health being minused slowly
+		health -= amount
+	if health <= 0:
+		die()
+
+func receive_debuff(amount: int): #Nothing happens ATM for the receive debuff because logic should be in game.gd script. However, function is present here because 
+	debuff_turn_counter = amount
+
+func minus_debuff_count():
+	debuff_turn_counter -= 1
+
+func debuff_count() -> int:
+	return debuff_turn_counter
+
 func die():
 	print("Archer has been defeated!")
 	queue_free()
+
+func play_animation(animation_name: String) -> void:
+	is_animating = true
+	animated_sprite.play(animation_name)
+	print("Playing animation:", animation_name)
+	await animated_sprite.animation_finished  # Await the end of the animation
+	print("Animation finished:", animation_name)
+	is_animating = false
+	animated_sprite.play("archer_idle")
+	print("Switched back to idle")
--- game-output-4/tank_unit.gd
+++ game-output-4/tank_unit.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-var health = 100
-var attack_1_damage = 35
-var attack_2_damage = 50
-var attack_3_damage = 75
+var health = 250
+var attack_1_damage = 20 
+var attack_2_damage = 25 
+var attack_3_damage = 30 
+var debuff_turn_counter = 0
 var is_animating = false
-
+  
+# Animations
 @onready var animated_sprite = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time
@@ -21,43 +23,31 @@ func _process(delta: float) -> void:
 func attack(attack_type: int) -> int:
 	match attack_type:
 		1:
-			play_animation("mage_attack_1")
-			return attack_1_damage
+			play_animation("tank_attack_1")
+			return attack_1_damage  # You can apply the damage after the animation is done
 		2:
-			play_animation("mage_attack_2")
+			play_animation("tank_attack_2")
 			return attack_2_damage
+		3:
+			play_animation("tank_attack_3")
+			return attack_3_damage
 		_:
 			print("Invalid attack type")
 			return 0
 
 func defend():
-	play_animation("mage_defend")
+	play_animation("tank_defend")
 
-func heal() -> int:
-	play_animation("mage_heal")
-	return 20
-	
-func poison() -> int:
-	play_animation("mage_poison")
-	return 3 #current amount of damage being dealt * 10
-
-func send_debuff() -> int:
-	var random_value = randi() % 100  # Generate a random number between 0 and 99
-	if random_value < 80:  # 0 to 79 (80% chance)
-		print("YOU MADE ENEMY FALL ASLEEP FOR 2 ROUNDS")
-		return 2
-	else:  # 80 to 99 (20% chance)
-		var oneortwo = randi() % 2 + 1
-		print("YOU MADE ENEMY FALL ASLEEP FOR " + str(oneortwo) + " ROUND/S")
-		return oneortwo  # Returns either 1 or 2
-	
 func take_damage(amount: int):
 	health -= amount
 	if health <= 0:
 		die()
 
+func damage_heal(add_health: int) -> void:
+	health += add_health
+
 func die():
-	print("Mage has been defeated!")
+	print("Warrior has been defeated!")
 	queue_free()
 
 func play_animation(animation_name: String) -> void:
